ec9418ad04fb940059e11b4b6507ff92aac8f49e
taylir <tdl9716@gmail.com>
2023-03-16 16:05:23 -0400

    Mostly final commit going to fix some QOL updates

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,68 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
-);
+import SongBar from "./SongBar";
+import { useState, useEffect } from "react";
+
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+  artistId,
+}) => {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "285110c9eamsh53fd7501a267f4fp1c913djsn6a7d37da6a81",
+        "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
+      },
+    };
+
+    fetch(
+      `https://shazam-core.p.rapidapi.com/v1/search/multi?query=${data?.attributes?.name}&search_type=SONGS`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setSongs(response.tracks.hits);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white">Realted Songs:</h2>
+      <div className="mt-6 w-full flex flex-col">
+        {artistId
+          ? songs?.map((song, i) => (
+              <SongBar
+                key={`${song.track.key}-${artistId}`}
+                song={song.track}
+                i={i}
+                artistId={artistId}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                handlePauseClick={handlePauseClick}
+                handlePlayClick={handlePlayClick}
+              />
+            ))
+          : data?.map((song, i) => (
+              <SongBar
+                key={`${song.key}-${artistId}`}
+                song={song}
+                i={i}
+                artistId={artistId}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                handlePauseClick={handlePauseClick}
+                handlePlayClick={handlePlayClick}
+              />
+            ))}
+      </div>
+    </div>
+  );
+};
 
 export default RelatedSongs;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,47 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "285110c9eamsh53fd7501a267f4fp1c913djsn6a7d37da6a81"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "v1/charts/world" }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `v1/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `v1/tracks/related?track_id=${songid}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `v2/artists/details?artist_id=${artistId}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `v1/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `v1/search/multi?query=${searchTerm}&search_type=SONGS_ARTISTS`,
+    }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsBySearchQuery,
+} = shazamCoreApi;
